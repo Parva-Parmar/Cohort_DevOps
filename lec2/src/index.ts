c020ba56f198331ee779181fa6e2de45bff880e0
Parva Parmar <parva004cp@gmail.com>
2026-01-08 12:10:48 +0530

    docker installation steps

--- lec2/src/index.ts
+++ lec2/src/index.ts
@@ -1,28 +1,33 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 import express from "express";
 
 const app = express();
-const prisma = new PrismaClient();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
   const data = await prisma.user.findMany();
-  res.json({
-    data
-  });
+  res.json({ data });
 });
 
-app.post("/", async(req, res) => {
+app.post("/", async (req, res) => {
   await prisma.user.create({
     data: {
       username: Math.random().toString(),
       password: Math.random().toString(),
     },
   });
-  res.json({
-    message: "Post endpoint",
-  });
+  res.json({ message: "Post endpoint" });
 });
 
 app.listen(3000);
